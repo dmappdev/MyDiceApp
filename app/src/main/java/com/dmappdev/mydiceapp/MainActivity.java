29369d1071f648d8277e6ef4287d522cf684893c
dmappdev <dmappdev@outlook.com>
2020-08-29 16:09:30 -0400

    first real commit added some layouts and started java coding

--- app/src/main/java/com/dmappdev/mydiceapp/MainActivity.java
+++ app/src/main/java/com/dmappdev/mydiceapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dmappdev.mydiceapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btnRoll = findViewById(R.id.btnRollTheDice);
+
+        btnRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("MyDiceApp", "btrRoll is tapped now");
+            }
+        });
+
     }
 }
