397e044809744a74407d0ab561d866d48ad7bbc1
dmappdev <dmappdev@outlook.com>
2020-08-30 09:33:05 -0400

    added sound rolling dice, added animation/library

--- app/src/main/java/com/dmappdev/mydiceapp/MainActivity.java
+++ app/src/main/java/com/dmappdev/mydiceapp/MainActivity.java
@@ -1,11 +1,18 @@
 package com.dmappdev.mydiceapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +24,42 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnRoll = findViewById(R.id.btnRollTheDice);
 
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.dice_sound);
+
         btnRoll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.i("MyDiceApp", "btrRoll is tapped now");
+
+                ImageView imgDice1 = findViewById(R.id.imgDice1);
+                ImageView imgDice2 = findViewById(R.id.imgDice2);
+
+                int[] diceImages = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
+
+
+                Random rndObject = new Random();
+                int myRandomNumber1 = rndObject.nextInt(6);// 0...5
+
+                Log.i("MyDiceApp", "Random n : " + myRandomNumber1);
+
+                imgDice1.setImageResource(diceImages[myRandomNumber1]);
+
+                myRandomNumber1 = rndObject.nextInt(6);// 0...5
+                imgDice2.setImageResource(diceImages[myRandomNumber1]);
+                YoYo.with(Techniques.Shake)
+                        .duration(400)
+                        .repeat(0)
+                        .playOn(imgDice1);
+                YoYo.with(Techniques.Shake)
+                        .duration(400)
+                        .repeat(0)
+                        .playOn(imgDice2);
+                mp.start();
+
+
             }
         });
+
 
     }
 }
